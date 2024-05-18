62f3e7c6666da1836ce915de2b73b3e82b3d10b6
Andrei Pronsky <andrey.pronsky@gmail.com>
2024-05-18 18:54:15 +0300

    DTO structure performed

--- src/main/java/com/pronsky/tickets/service/dto/FlightDto.java
+++ src/main/java/com/pronsky/tickets/service/dto/FlightDto.java
@@ -1,4 +1,35 @@
 package com.pronsky.tickets.service.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
 public class FlightDto {
+    private Long id;
+
+    private String origin;
+
+    private String originName;
+
+    private String destination;
+
+    private String destinationName;
+
+    private Date departureDate;
+
+    private Time departureTime;
+
+    private Date arrivalDate;
+
+    private Time arrivalTime;
+
+    private String carrier;
+
+    private byte stops;
+
+    private int price;
 }
